370ad7af0aababa953d9f06142cba41fcf40ddbe
Cserei Marton <warcodhiro@gmail.com>
2018-07-17 18:48:15 +0300

    add routing and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { routing } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavigationBarComponent, HomeComponent],
+  imports: [BrowserModule, routing],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
